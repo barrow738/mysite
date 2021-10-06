21a197d7ba94d90c88263fee6fd595169b3de034
barrow738 <57837415+barrow738@users.noreply.github.com>
2021-10-06 07:33:41 +0300

    Making mirgration

--- main/views.py
+++ main/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+# Create your views here.
 def index(response):
     return HttpResponse("<h1>Tech with tim tutorial</h1>")
 
+
 def view(response):
     return HttpResponse("<h1>This is the first view which is another page</h1>")
-# Create your views here.
