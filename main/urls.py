dd24b7a883366f75521c17d36838518fe7ee6dce
barrow738 <57837415+barrow738@users.noreply.github.com>
2021-10-07 06:01:23 +0300

    Dealing with templates

--- main/urls.py
+++ main/urls.py
@@ -8,5 +8,6 @@ from . import views
 
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path("<int:id>", views.index, name="index"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from .models import TodoList, Item
 
 # Create your views here.
+def home():
+
+
 def index(response, id):
     ls = TodoList.objects.get(id=id)
     return HttpResponse("<h1>%s</h1>" %ls.name)
