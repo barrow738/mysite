07f2ca890f73640959fdc67a4b43a50160de3559
barrow738 <57837415+barrow738@users.noreply.github.com>
2021-10-07 06:07:21 +0300

    Loading templates

--- main/views.py
+++ main/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 from .models import TodoList, Item
 
 # Create your views here.
-def home():
-
-
 def index(response, id):
     ls = TodoList.objects.get(id=id)
-    return HttpResponse("<h1>%s</h1>" %ls.name)
+    # return HttpResponse("<h1>%s</h1>" %ls.name)
+    return render(response, "main/base.html", {})
+
+
+def home(response):
+    return render(response, "main/home.html", {})
